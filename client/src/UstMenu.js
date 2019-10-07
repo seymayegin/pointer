5ce2fd8acd74644d25c97b46779ff64a8366a0b8
soner.kocak <soner.kocak@tarnet.com.tr>
2019-10-07 15:58:28 +0300

    UstMenu.js warning'ler düzeltildi.

--- client/src/UstMenu.js
+++ client/src/UstMenu.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
@@ -26,13 +23,8 @@ const useStyles = makeStyles(theme => ({
 
 export default function MenuAppBar() {
     const classes = useStyles();
-    const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
-
-    const handleChange = event => {
-        setAuth(event.target.checked);
-    };
 
     const handleMenu = event => {
         setAnchorEl(event.currentTarget);
@@ -52,7 +44,6 @@ export default function MenuAppBar() {
                     <Typography variant="h6" className={classes.title}>
                         Pointer
                     </Typography>
-                    {auth && (
                         <div>
                             <IconButton
                                 aria-label="account of current user"
@@ -81,7 +72,6 @@ export default function MenuAppBar() {
                                 <MenuItem onClick={handleClose}>Log Out</MenuItem>
                             </Menu>
                         </div>
-                    )}
                 </Toolbar>
             </AppBar>
         </div>
