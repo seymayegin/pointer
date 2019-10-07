0d8db4ce41f59dd4b4711bf6086941f3f095c494
soner.kocak <soner.kocak@tarnet.com.tr>
2019-10-07 16:17:29 +0300

    SolMenu.js eklendi.

--- client/src/UstMenu.js
+++ client/src/UstMenu.js
@@ -4,10 +4,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import SolMenu from './SolMenu';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -38,9 +38,7 @@ export default function MenuAppBar() {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
+                    <SolMenu/>
                     <Typography variant="h6" className={classes.title}>
                         Pointer
                     </Typography>
