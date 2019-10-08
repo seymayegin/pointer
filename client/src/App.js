18155799a747d6b68807daea708c3c582e8d7697
soner.kocak <soner.kocak@tarnet.com.tr>
2019-10-08 16:23:17 +0300

    Login eklendi.

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,30 @@ import UstMenu from "./UstMenu";
 import Sayfa1 from './Sayfa1';
 import Sayfa2 from './Sayfa2';
 import Home from './Home';
+import Login from './Login';
 
 const history = createBrowserHistory();
 
 function App() {
+
   return (
     <div>
         <Router history={history}>
-            <UstMenu history={history}/>
-
             <Switch>
-                <Route path={`/s1`} component={Sayfa1}/>
-                <Route path={`/s2`} component={Sayfa2}/>
-                <Route component={Home}/>
+                <Route path={`/login`} component={Login}/>
+                <Route>
+                    <Router history={history}>
+                        <UstMenu history={history}/>
+
+                        <Switch>
+                            <Route path={`/s1`} component={Sayfa1}/>
+                            <Route path={`/s2`} component={Sayfa2}/>
+                            <Route component={Home}/>
+                        </Switch>
+                    </Router>
+                </Route>
             </Switch>
         </Router>
-
-
     </div>
   );
 }
